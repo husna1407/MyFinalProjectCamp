eb206e757bd821e98f6bba68f4961d474f5168de
pc <pc@LAPTOP-IC7SA6CU>
2021-02-20 15:23:51 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity i implement eden class bir veritabanı ürünüdür. 
+    public interface IEntity
+    {
+
+    }
+}
