73037721ae7285536b230c41d4efa844affd8d71
pc <pc@LAPTOP-IC7SA6CU>
2021-02-24 20:53:10 +0300

    10.ders sonu

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages //static yapınca new lemiyoruz.
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz"; //public ler genelde pascal case isimlendirilir.
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        
+        public Result(bool success, string message):this(success)//"this" burada Result classını kasteder.
+            //:this(success)--> result ın tek parametreli constructor methoduna success i yolla.
+        {
+            Message = message;
+        }
+
+        public Result(bool success) //Overloading
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
